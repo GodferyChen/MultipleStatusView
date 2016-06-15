bfd79fdcc813000a82bd7e390ec86164088ff62e
陈修宏 <chenxh.cz@gmail.com>
2016-06-15 21:55:23 +0800

    显示加载中视图 显示空视图 显示错误视图 显示网络异常视图 显示内容视图

--- app/src/main/java/com/sample/chen/multiplestatusview/MainActivity.java
+++ app/src/main/java/com/sample/chen/multiplestatusview/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.clans.fab.FloatingActionMenu;
+import com.sample.chen.library.MultipleStatusView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MultipleStatusView mMultipleStatusView;
+    private FloatingActionMenu mFloatingActionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +25,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mMultipleStatusView = (MultipleStatusView) findViewById(R.id.multiple_status_view);
+        mFloatingActionMenu = (FloatingActionMenu) findViewById(R.id.main_fab_menu);
+        com.github.clans.fab.FloatingActionButton mLoadingFab = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.main_fab_loading);
+        com.github.clans.fab.FloatingActionButton mEmptyFab = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.main_fab_empty);
+        com.github.clans.fab.FloatingActionButton mErrorFab = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.main_fab_error);
+        com.github.clans.fab.FloatingActionButton mNoNetworkFab = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.main_fab_no_network);
+        com.github.clans.fab.FloatingActionButton mContentFab = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.main_fab_content);
+        mLoadingFab.setOnClickListener(this);
+        mEmptyFab.setOnClickListener(this);
+        mErrorFab.setOnClickListener(this);
+        mNoNetworkFab.setOnClickListener(this);
+        mContentFab.setOnClickListener(this);
+
+        mMultipleStatusView.setOnActionListener(new MultipleStatusView.OnActionListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onAction(View view) {
+                Toast.makeText(getBaseContext(), "OnActionListener", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -48,5 +66,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_fab_loading:
+                mMultipleStatusView.loading();
+                break;
+            case R.id.main_fab_empty:
+                mMultipleStatusView.empty();
+                break;
+            case R.id.main_fab_error:
+                mMultipleStatusView.custom(getResources().getDrawable(R.mipmap.ic_error),
+                        "error","retry");
+                break;
+            case R.id.main_fab_no_network:
+                mMultipleStatusView.custom(getResources().getDrawable(R.mipmap.ic_no_network),
+                        "no network","retry");
+                break;
+            case R.id.main_fab_content:
+                mMultipleStatusView.dismiss();
+                break;
+        }
+        mFloatingActionMenu.toggle(false);
     }
 }
